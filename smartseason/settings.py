824254db6aa7da5bbcd4edd3b10920859acc1b5d
samprinc <samprinc1122@gmail.com>
2026-04-26 10:34:08 +0300

    Add whitenoise for static file serving

--- smartseason/settings.py
+++ smartseason/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key-for-loca
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['smartseasonbnd.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['smartseasonbnd.onrender.com', 'localhost', '127.0.0.1', 'smartseasonbnd-4fe5.onrender.com']
 
 # Application definition
 
